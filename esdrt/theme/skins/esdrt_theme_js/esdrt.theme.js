a8aaa206624a9cb3db5622e805789b7d18d6dfdd
Luca Pisani <luca.pisani@abstract.it>
2016-05-26 17:02:37 +0200

    refs #72740: Introduce heading "draft/final conclusion flags...

--- esdrt/theme/skins/esdrt_theme_js/esdrt.theme.js
+++ esdrt/theme/skins/esdrt_theme_js/esdrt.theme.js
@@ -1,3 +1,15 @@
+function hightlight_restructured(){
+    if ($("body").hasClass("template-esdrt-content-observation") ||
+        $("body").hasClass("template-esdrt-content-conclusion") ||
+        $("body").hasClass("template-edit-highlights") ||
+        $("body").hasClass("template-edit portaltype-conclusion") ||
+        $("body").hasClass("template-esdrt-content-conclusion") ||
+        $("body").hasClass("template-edit portaltype-conclusionsphase2") ||
+        $("body").hasClass("template-view portaltype-observation")){
+            $("<br/><br/><span style='font-weight:bold'>Draft/final conclusion flags</span><br/>").insertBefore($("input[value='psi']").parent())
+    }
+}
+
 $(document).ready(function(){
     $(".collapsiblePanelTitle").click(function(e){
         var panel = $(this).data("panel");
@@ -30,13 +42,8 @@ $(document).ready(function(){
         var timeZone = time.substr(time.indexOf("+", time.length))
         timeAgo += " +0" + timeZone + ":00"
 		$(this).text(moment(timeAgo, "YYYY/MM/DD HH:mm:ss Z").fromNow())
-	})
-    if ($("body").hasClass("template-esdrt-content-observation") ||
-        $("body").hasClass("template-esdrt-content-conclusion") ||
-        $("body").hasClass("template-edit-highlights") ||
-        $("body").hasClass("template-edit portaltype-conclusion")){
-        $("<br/><br/><span style='font-weight:bold'>Draft/final conclusion flags</span><br/>").insertBefore($("input[value='psi']").parent())
-    }
+	});
+    hightlight_restructured();
     $('a.standardButton[title][title!=""]').addClass("tooltipIcon");
     /*
     if ($("body").hasClass("section-2015")) {
